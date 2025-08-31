90816eba506bc03c0a3986ab04cf5840b6056f1b
b1nk <me@b1nk.net>
2025-08-31 19:41:21 +0900

    feat: made it to electron app

---
+++ electron/main.js
@@ -0,0 +1,39 @@
+import { app, BrowserWindow } from "electron";
+import path from 'node:path'
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+process.env.APP_ROOT = path.join(__dirname, "..");
+
+export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron', 'main');
+export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist');
+
+process.env.VITE_DEV_SERVER_URL = process.env.VITE_DEV_SERVER_URL;
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        icon: path.join(process.env.APP_ROOT, 'public', 'favicon.ico'),
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    });
+
+    if (process.env.VITE_DEV_SERVER_URL) {
+        win.loadURL(process.env.VITE_DEV_SERVER_URL);
+        // win.webContents.openDevTools();
+    } else {
+        win.loadFile(path.join(RENDERER_DIST, 'index.html'));
+    }
+}
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+        win = null;
+    }
+});
+
+app.whenReady().then(createWindow);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import electron from 'vite-plugin-electron'
+import electronRenderer from 'vite-plugin-electron-renderer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    electron([
+      {
+        entry: 'electron/main.js',
+      },
+      {
+        entry: 'electron/preload.js',
+        onstart(options) {
+          options.reload()
+        },
+      },
+    ]),
+    electronRenderer(),
+  ],
 })
